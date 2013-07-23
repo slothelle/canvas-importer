0e11a44749c02bacc3112aaa07c66aed2c620f1f
Liz Abinante <eabinante@gmail.com>
2013-07-23 08:54:14 -0500

    fixed typo before running migration

--- db/migrate/20130723082122_create_students.rb
+++ db/migrate/20130723082122_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration
   def up
     create_table :students do |t|
-      t.sting :name, null: false
+      t.string :name, null: false
       t.string :user_id, null: false
       t.string :state, null: false
       t.timestamps
